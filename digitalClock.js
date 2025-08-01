6c8665bd4cd435e1379c2bd9025c538a04af2fd8
Sumairanishat <sumairanishat13@gmail.com>
2025-08-01 17:05:30 +0530

    Update digitalClock.js

--- digitalClock.js
+++ digitalClock.js
@@ -1,28 +1,3 @@
-/*let count=0;
-seconds=()=>{
-    let secondsText = document.getElementById('seconds').textContent;
-    let secondsInt = parseInt(secondsText);
-    console.log("sec");
-    secondsInt++;
-    document.getElementById('seconds').textContent=secondsInt;
-    count++;
-}
-minutes=()=>{
-  let minutesText= document.getElementById('minutes').textContent;
-  let minutesInt = parseInt(minutesText);
-  console.log('min');
-  minutesInt++;
- document.getElementById('minutes').textContent=minutesInt;
- count++
-}
-
-for(let i=1;i<=3;i++){
-  
-   setTimeout(seconds,1000*i);
-  
- 
-  
-}*/
 
 setInterval(() => {
   let date= new Date();
